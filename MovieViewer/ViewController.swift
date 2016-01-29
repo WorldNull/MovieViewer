cfa009d2de9748397d6f5201d319e16fb8c2db13
CandyPirate <worldnull@yahoo.com>
2016-01-28 19:35:49 -0500

    Initial Commit

---
+++ MovieViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieViewer
+//
+//  Created by YouGotToFindWhatYouLove on 1/28/16.
+//  Copyright © 2016 Candy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
